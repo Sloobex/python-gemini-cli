fd7011db4c5188d28532ac284936b6cb7c162a69
Sloobex <luras1411@gmail.com>
2026-04-20 17:19:58 +0200

    Initial commit - Gemini CLI chatbot

---
+++ main.py
@@ -0,0 +1,32 @@
+from google.genai import types
+import argparse
+import os
+from google import genai
+from dotenv import load_dotenv
+
+def main():
+    parser = argparse.ArgumentParser(description="Povezivanje sa Gemini AI")
+    parser.add_argument("user_prompt", type=str, help="Pitanje koje zelis da postavis AI modelu")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("Nije pronadjen GEMINI_API_KEY! Proveri da li tvoj .env fajl postoji i da li je ispravno napisan.")
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',contents = messages,
+    )
+    if response.usage_metadata is None:
+        raise RuntimeError("No data of tokens.")
+    print(response.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
