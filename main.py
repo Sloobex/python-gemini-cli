7bcd11b0f4067ac0d5786058d578187d4303c47b
Sloobex <luras1411@gmail.com>
2026-04-30 00:16:59 +0200

    feat: implement AI agent feedback loop and fix calculator bug

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import os
 from google import genai
 from dotenv import load_dotenv
+import time
 
 def main():
     parser = argparse.ArgumentParser(description="Povezivanje sa Gemini AI")
@@ -30,25 +31,32 @@ def main():
             print(f"User prompt: {args.user_prompt}")
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)  
         if response.function_calls:
+            function_results = []
             for function_call in response.function_calls:
                 print(f"Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call, verbose=True)
+                if not function_call_result.parts:
+                    raise Exception("Error: The '.parts' list in the function call result is empty.")
+                if function_call_result.parts[0].function_response is None:
+                    raise Exception("Error: 'function_response' is missing from the first part.")
+                if function_call_result.parts[0].function_response.response is None:
+                    raise Exception("Error: Actual 'response' data is missing from the function_response.")
+                verbose = True
+                function_results.append(function_call_result.parts[0])
+                if verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}") 
+            messages.append(types.Content(role="user", parts=function_results))
+            time.sleep(3)
         else:
             print(response.text)
-        function_results = []
-        function_call_result = call_function(function_call, verbose=True)
-        if not function_call_result.parts:
-            raise Exception("Error: The '.parts' list in the function call result is empty.")
-        if function_call_result.parts[0].function_response is None:
-            raise Exception("Error: 'function_response' is missing from the first part.")
-        if function_call_result.parts[0].function_response.response is None:
-            raise Exception("Error: Actual 'response' data is missing from the function_response.")
-        verbose = True
-        function_results.append(function_call_result.parts[0])
-        if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}") 
-    
-
+            break
+    else:
+        print("Critical error: Agent has reached the limit of 20 rounds and failed to complete the task!")   
+        exit(1)
 
 if __name__ == "__main__":
     main()
